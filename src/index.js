23cd0bfb05ad2c97056b82ebec489cc24ea2d469
Dan Flettre <flettre@gmail.com>
2018-01-17 14:53:01 -0600

    update events and combos

--- src/index.js
+++ src/index.js
@@ -26,19 +26,22 @@ var config = {
 }
 
 var state = {
-  tint: 0x77ff77,
-  rainbow: 0
+  tint: 0x55ff55,
+  rainbow: false,
+  rainbowIndex: 0,
+  spin: false
 }
 
 var tints = [
-  0x77ff77,
-  0xff7777,
-  0x7777ff,
-  0x77ffff,
-  0xffff77,
-  0xff77ff,
-  0xffffff,
-  0x777777
+  0xff70ff, // pink
+  0xff5555, // red
+  0xff8844, // orange
+  0xffff55, // yellow
+  0x55ff55, // green
+  0x00ffff, // blue
+  0x775599, // purple
+  0x555555, // grey
+  0xffffff // white
 ]
 
 function onResize () {
@@ -72,13 +75,15 @@ function create () {
   letterGroup = this.add.group()
 
   this.input.keyboard.on('keydown', function (event) {
+    // if (event.key.length === 1) console.log('event', event.key, event.key.charCodeAt())
+    var letter = this.physics.add.sprite(keys.KEYS_X[event.key.toUpperCase()], HEIGHT - 30)
 
     var vector = {
       x: (Math.floor((Date.now() / 200) % 10) / 200) - 0.025,
       y: -1 * (HEIGHT / 3200)
     }
 
-    letter.setTexture('letters', event.data.key.charCodeAt() + '.png')
+    letter.setTexture('letters', event.key.charCodeAt() + '.png')
     letter.setSizeToFrame()
     letter.setOrigin()
     letter.setBody(letter.x, letter.y, letter.width, letter.height)
@@ -86,25 +91,74 @@ function create () {
     letter.applyForce(vector)
     letter.setFriction(0.001, 0, 0)
     letter.setBounce(0.9)
+    if (state.spin) {
+      letter.setAngularVelocity((Math.random() / 2) * (Math.random() < 0.5 ? -1 : 1))
+    }
     letterGroup.add(letter)
-    letter.setTint(tints[state.rainbow])
-    letter.setMass(3.5)
-    state.rainbow = (state.rainbow + 1) % tints.length
+    letter.setDensity(letter.body.density * 1.8)
+    if (letter.body.mass < 2.5) letter.setMass(2.5)
+
+    if (state.rainbow) {
+      letter.setTint(tints[state.rainbowIndex])
+      state.rainbowIndex = (state.rainbowIndex + 1) % tints.length
+    } else {
+      letter.setTint(state.tint)
+    }
+
     fxLayer.add(letter)
   }.bind(this))
 
   fxLayer = this.add.effectLayer(0, 0, WIDTH, HEIGHT, 'bloom', bloom)
 
-    //  Here we'll create a simple key combo
-    //  When you type in ABCD the event will be triggered on the entry of the final character (D)
-    //  An incorrect key press will reset the combo back to the start again
+  var combos = {
+    pink: function (state) {
+      state.tint = tints[0]
+    },
+    red: function (state) {
+      state.tint = tints[1]
+    },
+    orange: function (state) {
+      state.tint = tints[2]
+    },
+    yellow: function (state) {
+      state.tint = tints[3]
+    },
+    green: function (state) {
+      state.tint = tints[4]
+    },
+    blue: function (state) {
+      state.tint = tints[5]
+    },
+    purple: function (state) {
+      state.tint = tints[6]
+    },
+    grey: function (state) {
+      state.tint = tints[7]
+    },
+    white: function (state) {
+      state.tint = tints[8]
+    },
+    rainbow: function (state) {
+      state.rainbow = !state.rainbow
+    },
+    spin: function (state) {
+      state.spin = !state.spin
+    },
+    fall: function (state) {
+      state.fall = !state.fall
+    }
+  }
+  combos.feross = combos.spin
+  combos.gray = combos.grey
+  combos.violet = combos.purple
 
-  var combo = this.input.keyboard.createCombo('ABCD')
+  var self = this
+  Object.keys(combos).forEach(function (combo) {
+    self.input.keyboard.createCombo(combo, {resetOnMatch: true}).label = combo
+  })
 
-  this.input.keyboard.events.on('KEY_COMBO_MATCH_EVENT', function (event) {
-    console.log('Key Combo matched!')
-    console.log('event.data:', event.data)
-    console.log('event.target:', event.target)
+  this.input.keyboard.on('keycombomatch', function (event) {
+    if (combos[event.label]) combos[event.label](state)
   })
 }
 
